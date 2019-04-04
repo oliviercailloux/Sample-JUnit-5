a932535cb936f84fc873671d60976766ae539098
Olivier Cailloux <olivier.cailloux@dauphine.fr>
2019-04-04 17:13:07 +0200

    Explicit access to class

--- src/test/java/io/github/oliviercailloux/sample_junit_5/SuperComputerTests.java
+++ src/test/java/io/github/oliviercailloux/sample_junit_5/SuperComputerTests.java
@@ -1,7 +1,6 @@
 package io.github.oliviercailloux.sample_junit_5;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class SuperComputerTests {
@@ -10,7 +9,7 @@ class SuperComputerTests {
 	void testComplicatedComputationWithParameter0() {
 		SuperComputer superComputer = new SuperComputer();
 		int obtained = superComputer.complicatedComputation(0);
-		assertEquals(3, obtained);
+		Assertions.assertEquals(3, obtained);
 	}
 
 }
